72356b41cc53cd3ed6ed4f2e50943503be4c5c20
Filip <fpodsiadly@gmail.com>
2025-04-17 21:06:02 +0200

    Fix vite.config.js and vercel.json: update base path and route destination for frontend assets

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Zmieniono bazową ścieżkę na root
+  base: 'frontend/', // Bazowa ścieżka dla zasobów
 })
